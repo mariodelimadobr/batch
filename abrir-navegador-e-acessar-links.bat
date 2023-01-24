dca9ed49870df4431ae3d2611607cc990bf2bf7e
Mário de Lima <mariodelimadobrasil@gmail.com>
2023-01-24 07:33:11 -0300

    Create abrir-navegador-e-acessar-links.bat

---
+++ abrir-navegador-e-acessar-links.bat
@@ -0,0 +1,9 @@
+@echo off
+
+start msedge.exe https://intranet.joinville.sc.gov.br/capacitacao/formulario/csv/cod_formulario/1220
+start msedge.exe https://intranet.joinville.sc.gov.br/capacitacao/formulario/csv/cod_formulario/1231
+
+rem start chrome https://intranet.joinville.sc.gov.br/capacitacao/formulario/csv/cod_formulario/1220
+rem start chrome https://intranet.joinville.sc.gov.br/capacitacao/formulario/csv/cod_formulario/1231
+
+exit
